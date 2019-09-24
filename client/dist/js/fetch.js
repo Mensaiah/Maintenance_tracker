8806c6414d5dc57fe46b2b52b473416811890cbf
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-09-24 14:23:48 +0100

    HTTP library completed

--- client/dist/js/fetch.js
+++ client/dist/js/fetch.js
@@ -3,23 +3,79 @@ const header = {
   "x-auth-token": localStorage.token
 };
 
-// class Fetch {
-//   async get(url, cbFunction) {
-//     try {
-//       this.url = url;
-//       const response = await fetch(url, {
-//         method: "GET",
-//         headers: header
-//       });
+class Fetch {
+  // HTTP GET Request
+  async get(url, cbFunction) {
+    try {
+      this.url = url;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: header
+      });
+      return {
+        data: await response.json(),
+        status: await response.status
+      };
+    } catch (error) {
+      console.log(error);
+    }
+    cbFunction();
+  }
 
-//       return {
-//         data: response.json(),
-//         status: response.status
-//       };
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     cbFunction();
-//     return
-//   }
-// }
+  // HTTP Post Request
+  async post(url, data, cbFunction) {
+    try {
+      this.url = url;
+      this.data = data;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: header,
+        data: JSON.stringify(data)
+      });
+      return {
+        data: await response.json(),
+        status: await response.status
+      };
+    } catch (error) {
+      console.log(error);
+    }
+    cbFunction();
+  }
+
+  async put(url, data, cbFunction) {
+    try {
+      this.url = url;
+      this.data = data;
+      const response = await fetch(url, {
+        method: "PuT",
+        headers: header,
+        data: JSON.stringify(data)
+      });
+      return {
+        data: await response.json(),
+        status: await response.status
+      };
+    } catch (error) {
+      console.log(error);
+    }
+    cbFunction();
+  }
+
+  async delete(url, cbFunction) {
+    try {
+      this.url = url;
+
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: header
+      });
+      return {
+        data: await response.json(),
+        status: await response.status
+      };
+    } catch (error) {
+      console.log(error);
+    }
+    cbFunction();
+  }
+}
